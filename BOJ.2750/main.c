e1247bd09681fd1e48ace85faf43b918b58d5c05
Leeseojune <61784568+leeseojune53@users.noreply.github.com>
2020-04-18 18:15:49 +0900

    Create main.c

---
+++ BOJ.2750/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,tmp;
+    int *arr;
+    scanf("%d",&n);
+    arr=(int*)malloc(sizeof(int)*n);
+    for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(arr[i]>arr[j]){
+                tmp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tmp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+        printf("%d\n",arr[i]);
+    return 0;
+}
