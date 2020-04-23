36f468ac32516e4ec27c38ef451facc7a41880e8
Leeseojune <61784568+leeseojune53@users.noreply.github.com>
2020-04-24 01:01:55 +0900

    Create main.cpp

---
+++ BOJ/BOJ.3053/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int R;
+    cin >> R;
+    cout << fixed;
+    cout.precision(6);
+    cout << 3.14159265358979*R*R << "\n";
+    cout << 2*R*R;
+    return 0;
+}
