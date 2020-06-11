2107411326569bc57459d004a8d87e4c5ee7a3f7
leeseojune53 <sung07288346@gmail.com>
2020-06-11 17:36:12 +0900

    print Hell world, struct d.x

--- Cs_learn/ConsoleApp1/Program.cs
+++ Cs_learn/ConsoleApp1/Program.cs
@@ -8,8 +8,15 @@ namespace ConsoleApp1
 {
     class Program
     {
+        private struct d
+        {
+            public static int x = 1;
+        }
         static void Main(string[] args)
         {
+            d D = new d();
+            Console.WriteLine("Hell world");
+            Console.WriteLine(d.x);
         }
     }
 }
