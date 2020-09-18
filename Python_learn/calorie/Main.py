dd839bdaf537217aa74878e59bfc085a1c128e70
leeseojune <sung07288346@gmail.com>
2020-09-19 02:31:50 +0900

    2020/09/19

---
+++ Python_learn/calorie/Main.py
@@ -0,0 +1,13 @@
+# 여기에 코드를 작성하세요.
+kitkat = 190
+oreos = 502
+pringles = 292
+twix = 135.9
+cheetos = 485
+
+
+# 다양한 과자 조합
+print(kitkat + oreos * 2)
+print(cheetos * 4)
+print(pringles + oreos + twix)
+print(pringles * 3 + oreos * 2)
