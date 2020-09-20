62437e9b0a635c45588e25fa7da2fa872ec8bfe1
leeseojune <sung07288346@gmail.com>
2020-09-21 08:58:07 +0900

    2020/09/21

---
+++ Android/app/src/main/java/com/example/mywork00/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mywork00;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
