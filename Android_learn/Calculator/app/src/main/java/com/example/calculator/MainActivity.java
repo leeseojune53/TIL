079a3287709eb18679c30890c8455a5e22b1d486
leeseojune <sung07288346@gmail.com>
2020-10-02 02:53:15 +0900

    2020/10/02

--- Android_learn/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Android_learn/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View.OnClickListener cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        }
     }
 }
