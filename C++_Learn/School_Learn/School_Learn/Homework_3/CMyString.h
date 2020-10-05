91ed5162e2faf71d3aae5993e63d012e81c6b5df
leeseojune <sung07288346@gmail.com>
2020-10-05 23:34:27 +0900

    2020/10/05

--- C++_Learn/School_Learn/School_Learn/Homework_3/CMyString.h
+++ C++_Learn/School_Learn/School_Learn/Homework_3/CMyString.h
@@ -1,1 +1,25 @@
 #pragma once
+
+class CMyString {
+private:
+	string str;
+public:
+	void SetString(string s_parm) {
+		str = s_parm;
+	}
+
+	string GetString() { return str; }
+
+	CMyString(const CMyString& rhs)
+		//	:	m_nData(rhs.m_nData)
+	{
+		this->str = rhs.str;
+		cout << "CMyData(const	CMyData	&)" << endl;
+	}
+
+	CMyString& operator=(const CMyString& rhs) {
+		*str = *rhs.str;
+		//	객체	자신에	대한	참조를	반환한다.
+		return	*this;
+	}
+};
