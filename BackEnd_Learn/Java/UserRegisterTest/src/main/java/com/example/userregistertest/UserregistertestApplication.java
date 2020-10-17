c9f980ca1063592e56de8df2f02c6a305bbdbae9
leeseojune <sung07288346@gmail.com>
2020-10-17 17:16:53 +0900

    based

---
+++ BackEnd_Learn/Java/UserRegisterTest/src/main/java/com/example/userregistertest/UserregistertestApplication.java
@@ -0,0 +1,13 @@
+package com.example.userregistertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserregistertestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserregistertestApplication.class, args);
+    }
+
+}
---
+++ BackEnd_Learn/Java/UserRegisterTest/src/test/java/com/example/userregistertest/UserregistertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userregistertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
