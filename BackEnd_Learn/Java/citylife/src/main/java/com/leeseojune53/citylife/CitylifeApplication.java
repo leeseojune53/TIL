9f7a697f1ea9be6695fec6183716d9591a968ca3
leeseojune <sung07288346@gmail.com>
2020-10-24 16:42:40 +0900

    2020/10/24

---
+++ BackEnd_Learn/Java/citylife/src/main/java/com/leeseojune53/citylife/CitylifeApplication.java
@@ -0,0 +1,13 @@
+package com.leeseojune53.citylife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitylifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitylifeApplication.class, args);
+    }
+
+}
---
+++ BackEnd_Learn/Java/citylife/src/test/java/com/leeseojune53/citylife/CitylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeseojune53.citylife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitylifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
