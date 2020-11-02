08c106c662fcd1b815153d6965c9f0a802b688aa
leeseojune <sung07288346@gmail.com>
2020-11-02 23:42:02 +0900

    2020/11/02

---
+++ C++_Learn/School_Learn/School_Learn/CMyData_/CMyData_.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class CMyDataArr
+{
+private:
+    int* m_pnArr = nullptr;
+    int count = -1;
+    int Size;
+public:
+    CMyDataArr(int Size) {
+        m_pnArr = new int[Size];
+        this->Size = Size;
+    }
+
+    void Insert(int Data) {
+        if (m_pnArr == NULL) {
+            cout << "m_pnArr is NULL" << endl;
+            return;
+        }
+        if (count == Size - 1) {
+            cout << "m_pnArr is FULL" << endl;
+            return;
+        }
+        m_pnArr[++count] = Data;
+    }
+
+    void PrintData() {
+        if (m_pnArr == NULL) {
+            cout << "m_pnArr is NULL" << endl;
+            return;
+        }
+        cout << "전체 데이터 : ";
+        for (int i = 0; i < count + 1; i++) {
+            cout << m_pnArr[i] << " ";
+        }
+
+        cout << " [" << (count + 1) << " / " << Size << " 개 ]" << endl;
+    }
+};
+
+int	main()
+{
+    CMyDataArr	arr(10);
+    arr.Insert(10);
+    arr.Insert(96);
+    arr.Insert(-45);
+    arr.Insert(90);
+    arr.Insert(-21);
+    arr.Insert(100);
+    arr.Insert(63);
+    arr.Insert(28);
+    arr.Insert(14);
+    arr.Insert(48);
+    arr.PrintData();
+    return	0;
+}
---
+++ C++_Learn/School_Learn/School_Learn/CMyStrArr/CMyStrArr.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+class CMyStrArr
+{
+private:
+    char** m_pszStr = nullptr;
+    int Size;
+    int count = -1;
+public:
+    CMyStrArr(int size) {
+        this->Size = size;
+        m_pszStr = new char* [Size];
+    }
+
+    void Insert(const char* Data) {
+        if (Data == NULL) {
+            cout << "Data is Empty";
+            return;
+        }
+        if (m_pszStr == NULL) {
+            cout << "m_pszStr is NULL";
+            return;
+        }
+        m_pszStr[++count] = (char*)Data;
+        //int Data_size = strlen(Data);
+        //m_pszStr[++count] = new char[Data_size + 1];
+        //strcpy(m_pszStr[count], Data);
+
+    }
+
+    void PrintData() {
+        if (m_pszStr == NULL) {
+            cout << "m_pszStr is NULL";
+            return;
+        }
+        cout << "전체 데이터   [ " << (count + 1) << " / " << Size << "개 ]" << endl;
+        for (int i = 0; i < count + 1; i++) {
+            cout << i + 1 << " : " << m_pszStr[i] << endl;
+        }
+    }
+
+};
+
+int	main()
+{
+    CMyStrArr	arr(10);
+    arr.Insert("월요일마다고고");
+    arr.Insert("가나다123");
+    arr.Insert("홍길동	abc");
+    arr.Insert("함수형	!!");
+    arr.Insert("알고리즘	zzz");
+    arr.Insert("Tasla	Linux");
+    arr.PrintData();
+
+    return	0;
+}
