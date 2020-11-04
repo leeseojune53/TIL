dcd9cc0459f5e85c3d9d1dd051e41e28b346b432
leeseojune <sung07288346@gmail.com>
2020-11-05 08:38:49 +0900

    .

--- BackEnd_Learn/Java/citylife/src/main/java/com/leeseojune53/citylife/Repository/UserRepository.java
+++ BackEnd_Learn/Java/citylife/src/main/java/com/leeseojune53/citylife/Repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.leeseojune53.citylife.Repository;
 
 import com.leeseojune53.citylife.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
-    public List<User> findTop3ByOrderByUserIdDesc();
+    List<User> findTop3ByOrderByUserIdDesc();
 }
