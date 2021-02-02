d25ceb055afabd7b4e4abf8cd8448e8295775fd1
leeseojune <sung07288346@gmail.com>
2021-02-02 19:39:25 +0900

    CREATE; SecurityConfig

---
+++ BackEnd_Learn/Java/sms/src/main/java/com/lee/sms/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.lee.sms.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .cors().and()
+                .sessionManagement().disable()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
