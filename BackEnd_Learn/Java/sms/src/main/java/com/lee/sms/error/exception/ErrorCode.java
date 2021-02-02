3a10826adde37098d8db01e0db3ab19a2460216d
leeseojune <sung07288346@gmail.com>
2021-02-02 19:38:31 +0900

    CREATE; ErrorCode

---
+++ BackEnd_Learn/Java/sms/src/main/java/com/lee/sms/error/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.lee.sms.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum ErrorCode {
+
+    NUMBER_NOT_EXIST(404, "verify number not exist."),
+    EXPIRED_AUTH_CODE(400, "verify was expired."),
+    NUMBER_NOT_MATCH(400, "verify number not match");
+
+    private final int status;
+    private final String message;
+}
