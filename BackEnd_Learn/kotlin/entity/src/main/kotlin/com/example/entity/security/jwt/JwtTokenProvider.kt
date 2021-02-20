9ec7819370244b2fa4f020eead3a60637556d3db
leeseojune <sung07288346@gmail.com>
2021-02-20 21:01:42 +0900

    UPDATE; JwtTokenProvider

--- BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/security/jwt/JwtTokenProvider.kt
+++ BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/security/jwt/JwtTokenProvider.kt
@@ -1,17 +1,43 @@
 package com.example.entity.security.jwt
 
 import com.example.entity.exception.InvalidTokenException
+import com.example.entity.security.jwt.auth.AuthDetails
+import com.example.entity.security.jwt.auth.AuthDetailsService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.util.*
 import javax.servlet.http.HttpServletRequest
 
-class JwtTokenProvider {
+@Component
+class JwtTokenProvider(private val authDetailsService: AuthDetailsService) {
 
     @Value("\${jwt.header}")
     var header: String? = null
 
     @Value("\${jwt.prefix}")
     var prefix: String = ""
+
+    @Value("\${jwt.secret}")
+    var secret: String = ""
+
+    @Value("\${jwt.exp.access}")
+    var accessExp: Long = 0L
+
+    fun generateAccessToken(id: Int): String{
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, getSigningKey())
+                .setHeaderParam("typ", "JWT")
+                .setSubject(id.toString())
+                .claim("type", "access")
+                .setIssuedAt(Date())
+                .setExpiration(Date(System.currentTimeMillis() + accessExp * 1000))
+                .compact()
+    }
 
     fun resolveToken(request: HttpServletRequest) : String?{
         val bearer: String= request.getHeader(header)
@@ -24,9 +50,30 @@ class JwtTokenProvider {
     }
 
     fun validateToken(token:String) : Boolean{
-        try{
-            O
+        return try{
+            Jwts.parser().parseClaimsJws(token).body.subject
+            true
+        }catch (e: Exception){
+            false
         }
+    }
+
+    fun authentication(token: String) : Authentication{
+        val authDetails: AuthDetails = authDetailsService.loadUserByUsername(getId(token))
+        return UsernamePasswordAuthenticationToken(authDetails, "", authDetails.authorities)
+    }
+
+    private fun getId(token: String) : String{
+        return try{
+            Jwts.parser()
+                    .setSigningKey(getSigningKey()).parseClaimsJws(token).body.subject
+        }catch (e: Exception){
+            throw InvalidTokenException()
+        }
+    }
+
+    private fun getSigningKey() : String{
+        return Base64.getEncoder().encodeToString(secret.encodeToByteArray())
     }
 
 }
