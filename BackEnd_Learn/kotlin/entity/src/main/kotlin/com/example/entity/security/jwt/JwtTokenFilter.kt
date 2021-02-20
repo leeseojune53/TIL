3a9d6721f1ef7309f2582d30477dcc1ea97e1fc6
leeseojune <sung07288346@gmail.com>
2021-02-20 21:01:34 +0900

    UPDATE; JwtTokenFilter

--- BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/security/jwt/JwtTokenFilter.kt
+++ BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/security/jwt/JwtTokenFilter.kt
@@ -1,5 +1,7 @@
 package com.example.entity.security.jwt
 
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.GenericFilterBean
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
@@ -10,9 +12,23 @@ class JwtTokenFilter(val jwtTokenProvider: JwtTokenProvider): GenericFilterBean(
 
 
     override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        try{
-            var token: String = jwtTokenProvider.resolveToken(request as HttpServletRequest)
-            if (token != null && )
+        request ?: throw Exception()
+        val token: String? = jwtTokenProvider.resolveToken(request as HttpServletRequest)
+        token?.let {
+            if(jwtTokenProvider.validateToken(token)) {
+                val auth: Authentication = jwtTokenProvider.authentication(token)
+                SecurityContextHolder.getContext().authentication = auth
+            }
         }
+        chain?.doFilter(request, response)
+//        var token: String? = jwtTokenProvider.resolveToken(request as HttpServletRequest)
+//        try{
+//
+//            if (token != null && jwtTokenProvider.validateToken(token)) run {
+//                val auth: Authentication = jwtTokenProvider.authentication(token)
+//                SecurityContextHolder.getContext().authentication = auth
+//            }
+//        }
+
     }
 }
