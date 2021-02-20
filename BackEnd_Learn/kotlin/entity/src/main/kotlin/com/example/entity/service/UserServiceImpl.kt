3915f4ac402904894a539e7cd68b583b94fce228
leeseojune <sung07288346@gmail.com>
2021-02-20 21:01:53 +0900

    UPDATE; UserServiceImpl

--- BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserServiceImpl.kt
+++ BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserServiceImpl.kt
@@ -3,20 +3,48 @@ package com.example.entity.service
 import com.example.entity.dto.UserDTO
 import com.example.entity.entity.user.User
 import com.example.entity.entity.user.UserRepository
+import com.example.entity.exception.InvalidTokenException
+import com.example.entity.security.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.lang.RuntimeException
 
 @Service
-class UserServiceImpl (val userRepository: UserRepository, val passwordEncoder: PasswordEncoder) : UserService {
+class UserServiceImpl (val userRepository: UserRepository,
+                       val passwordEncoder: PasswordEncoder,
+                       val jwtTokenProvider: JwtTokenProvider,
+                       val authenticationManager: AuthenticationManager) : UserService {
 
 
     override fun register(request: UserDTO.UserInfo) {
-        userRepository.save(User(request.name, passwordEncoder.encode(request.password)));
+        userRepository.save(User(passwordEncoder.encode(request.password), request.name));
     }
 
     override fun login(request: UserDTO.UserInfo) {
+        var user: User = userRepository.findByName(request.name).get()
+
+
         userRepository.findByName(request.name)
-                .map { t: User -> passwordEncoder.matches(request.password, t.password) }
+                .filter{ t: User -> passwordEncoder.matches(request.password, t.password) }
+                .map { t: User -> t.id }
+                .map { t: Int ->
+                    try{
+                        authenticationManager.authenticate(getAuthToken(t, request.password))
+                    }catch (e: Exception){
+                        print(e.stackTrace.toString())
+                        throw InvalidTokenException()
+                    }
+                    val accessToken: String = jwtTokenProvider.generateAccessToken(t);
+                    println(accessToken)
+                 }
+                .orElseThrow()
+    }
+
+    private fun getAuthToken(id: Int, pwd: String) : UsernamePasswordAuthenticationToken{
+        return UsernamePasswordAuthenticationToken(id, pwd)
     }
 }
