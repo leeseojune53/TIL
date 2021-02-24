58dbd4d67967c06fe52f1ffb71f9493025aa86f2
leeseojune <sung07288346@gmail.com>
2021-02-25 03:14:27 +0900

    UPDATE; UserService

--- BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserService.kt
+++ BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserService.kt
@@ -4,5 +4,5 @@ import com.example.entity.dto.UserDTO
 
 interface UserService {
     fun register(request: UserDTO.UserInfo)
-    fun login(request: UserDTO.UserInfo)
+    fun login(request: UserDTO.UserInfo) : UserDTO.Token
 }
