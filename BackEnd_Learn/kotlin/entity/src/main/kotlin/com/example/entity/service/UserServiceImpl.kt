b13512df5244c0229e710c1aa3640d92e8506644
leeseojune <sung07288346@gmail.com>
2021-02-25 03:14:38 +0900

    UPDATE; UserServiceImpl

--- BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserServiceImpl.kt
+++ BackEnd_Learn/kotlin/entity/src/main/kotlin/com/example/entity/service/UserServiceImpl.kt
@@ -1,34 +1,43 @@
 package com.example.entity.service
 
 import com.example.entity.dto.UserDTO
+import com.example.entity.entity.token.Token
+import com.example.entity.entity.token.TokenRepository
 import com.example.entity.entity.user.User
 import com.example.entity.entity.user.UserRepository
 import com.example.entity.exception.InvalidTokenException
 import com.example.entity.security.jwt.JwtTokenProvider
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
-import java.lang.RuntimeException
 
 @Service
 class UserServiceImpl (val userRepository: UserRepository,
+                       //val tokenRepository: TokenRepository,
                        val passwordEncoder: PasswordEncoder,
                        val jwtTokenProvider: JwtTokenProvider,
                        val authenticationManager: AuthenticationManager) : UserService {
+
+    @Value("\${jwt.exp.access}")
+    var accessExp: Long = 0
+
+    @Value("\${jwt.exp.refresh}")
+    var refreshExp: Long = 0
+
 
 
     override fun register(request: UserDTO.UserInfo) {
         userRepository.save(User(passwordEncoder.encode(request.password), request.name));
     }
 
-    override fun login(request: UserDTO.UserInfo) {
+    override fun login(request: UserDTO.UserInfo) : UserDTO.Token {
         var user: User = userRepository.findByName(request.name).get()
+        var accessToken: String
+        var refreshToken: String
 
-
-        userRepository.findByName(request.name)
+        return userRepository.findByName(request.name)
                 .filter{ t: User -> passwordEncoder.matches(request.password, t.password) }
                 .map { t: User -> t.id }
                 .map { t: Int ->
@@ -38,10 +47,14 @@ class UserServiceImpl (val userRepository: UserRepository,
                         print(e.stackTrace.toString())
                         throw InvalidTokenException()
                     }
-                    val accessToken: String = jwtTokenProvider.generateAccessToken(t);
-                    println(accessToken)
+                    accessToken = jwtTokenProvider.generateAccessToken(t)
+                    refreshToken = jwtTokenProvider.generateRefreshToken(t)
+                    //tokenRepository.save(Token(refreshToken, refreshExp * 1000))
+                    UserDTO.Token(accessToken, refreshToken, accessExp * 1000)
                  }
                 .orElseThrow()
+
+
     }
 
     private fun getAuthToken(id: Int, pwd: String) : UsernamePasswordAuthenticationToken{
