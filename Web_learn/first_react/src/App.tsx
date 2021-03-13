34dd264b9267e1115d1b748c8279a88ceae44f91
leeseojune <sung07288346@gmail.com>
2021-03-13 21:50:28 +0900

    CREATE; React

---
+++ Web_learn/first_react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './component/test';
+
+function App() {
+  return (
+    <Test data = "이서준"></Test>
+  )
+}
+
+export default App;
---
+++ Web_learn/first_react/src/component/test.tsx
@@ -0,0 +1,11 @@
+
+const Test = ({data}:{data:string}) =>{
+    return (
+        <div>
+            <h1>First React</h1>
+            <h2>{data}</h2>
+        </div>
+    )
+}
+
+export default Test;
