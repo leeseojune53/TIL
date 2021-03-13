eb5f39425dd298a9b35907adb737a37be0e587dd
leeseojune <sung07288346@gmail.com>
2021-03-13 21:50:16 +0900

    CREATE; python

--- Python_learn/CodeitPython/test/funclearn.py
+++ Python_learn/CodeitPython/test/funclearn.py
@@ -1,5 +1,13 @@
-def hello(name):
-    print(name)
+print("이름을 입력하세요 : ")
+name = input()
+print("월급을 입력하세요 : ")
+a = int(input())
+print("금리를 입력하세요 : ")
+b = float(input()) / 100
 
-hello("asdf")
-hello("asdf")
+C = 15.4
+
+result = ((a/2) * b) * 12
+
+print(f'{name}씨의 정기예금 만기 금액은 {int(a / 2 * 12 + result):,}')
+
---
+++ Python_learn/CodeitPython/test/time.py
@@ -0,0 +1,21 @@
+# List = [[1, 2], [3, 4], [5, 6]]
+#
+# for first, second in List:
+#     print('===================================')
+#     print(f'first * second = {first * second}')
+#     print(f'first + second = {first + second}')
+
+# list = [[0 for x in range(10)]for y in range(10)]
+# for i in range(10):
+#     list[i][i] = 1
+#
+# for i in range(10):
+#     print(list[i])
+
+# def count_lenth(word: str, num: int) -> int:
+#     return 1
+# seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+# print(dir())
+
+for i in range(100000000000000000000000000000):
+    print(i)
