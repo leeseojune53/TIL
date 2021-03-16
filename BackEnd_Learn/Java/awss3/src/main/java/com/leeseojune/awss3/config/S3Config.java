541c3d399db91104dc66593aab42d70930ea2c2f
leeseojune <sung07288346@gmail.com>
2021-03-16 20:20:15 +0900

    CREATE; S3Config

---
+++ BackEnd_Learn/Java/awss3/src/main/java/com/leeseojune/awss3/config/S3Config.java
@@ -0,0 +1,34 @@
+package com.leeseojune.awss3.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 getS3ClientBean() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+}
