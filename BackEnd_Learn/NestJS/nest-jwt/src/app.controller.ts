1df8d2585ce2048cccdd0646063a1da3d5cf3103
leeseojune <sung07288346@gmail.com>
2021-03-20 23:26:23 +0900

    UPDATE; nest-jwt

--- BackEnd_Learn/NestJS/nest-jwt/src/app.controller.ts
+++ BackEnd_Learn/NestJS/nest-jwt/src/app.controller.ts
@@ -1,11 +1,24 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { UserDto } from './users/users.dto';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
+  constructor(
+    private readonly authService: AuthService
+  ){}
+
+  // @UseGuards(AuthGuard('local'))
+  // @Post('auth/login')
+  // async login(@Request() req) {
+  //   return req.user;
+  // }
+
+  @Post('login')
+  async login(@Body() userDto: UserDto) {
+    return this.authService.login(1);
   }
+
 }
--- BackEnd_Learn/NestJS/nest-jwt/src/app.module.ts
+++ BackEnd_Learn/NestJS/nest-jwt/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { User } from './users/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule, 
+    AuthModule,
+    JwtModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: 'lee@1234',
+      port: 3306,
+      database: 'nodejs',
+      entities: [User],
+      synchronize: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
