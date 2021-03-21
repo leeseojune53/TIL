6b8d0ce600f68ed8c8b7763e2caad89d5c5978d3
leeseojune <sung07288346@gmail.com>
2021-03-21 23:41:39 +0900

    NestJS

---
+++ BackEnd_Learn/NestJS/nomadcoders/first-nest/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
+
+@Module({
+  imports: [],
+  controllers: [MoviesController],
+  providers: [MoviesService],
+})
+export class AppModule {}
