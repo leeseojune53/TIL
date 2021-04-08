9579bb7f474016d13f85d1362e710ab606ad6c48
leeseojune <sung07288346@gmail.com>
2021-04-08 16:12:47 +0900

    CREATE; ImageController

---
+++ BackEnd_Learn/Java/image/src/main/java/com/leeseojune/image/controller/ImageController.java
@@ -0,0 +1,33 @@
+package com.leeseojune.image.controller;
+
+import com.leeseojune.image.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping("/upload")
+    public void uploadImage(@RequestParam("file")MultipartFile file) throws IOException {
+        imageService.upload(file);
+    }
+
+    @GetMapping(
+            value = "/image",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public byte[] getImage(@RequestParam("path") String path) throws IOException {
+        return imageService.getImage(path);
+    }
+
+}
