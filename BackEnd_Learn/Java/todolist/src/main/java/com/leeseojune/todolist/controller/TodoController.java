6972de876eaa20a11d2370e87dffe8c75e576b69
leeseojune <sung07288346@gmail.com>
2021-04-12 23:15:41 +0900

    UPDATE; TodoController

--- BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/controller/TodoController.java
+++ BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/controller/TodoController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
