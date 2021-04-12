8c8aec7eefd03c37a36ab37d447c2814f5cae211
leeseojune <sung07288346@gmail.com>
2021-04-12 19:45:39 +0900

    CREATE; BusinessException

---
+++ BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/error/excetion/BusinessException.java
@@ -0,0 +1,15 @@
+package com.leeseojune.todolist.error.excetion;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+
+    private ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode){
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
