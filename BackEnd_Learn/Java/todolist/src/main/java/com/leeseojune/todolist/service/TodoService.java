f3bac88dcbd345bb7d25a370f9c08e460016899f
leeseojune <sung07288346@gmail.com>
2021-04-12 19:46:08 +0900

    CREATE; TodoService

---
+++ BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/service/TodoService.java
@@ -0,0 +1,12 @@
+package com.leeseojune.todolist.service;
+
+import com.leeseojune.todolist.dto.TodoDTO;
+
+import java.util.List;
+
+public interface TodoService {
+    List<TodoDTO.Todo> getAll();
+    void addTodo(TodoDTO.Add request);
+    void check(int todoId);
+    void delete(int todoId);
+}
