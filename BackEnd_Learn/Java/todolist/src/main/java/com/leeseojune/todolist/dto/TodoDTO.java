2dcdd322c1b5371109201faeadcc9886b53f0f9e
leeseojune <sung07288346@gmail.com>
2021-04-12 19:45:12 +0900

    CREATE; TodoDTO

---
+++ BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/dto/TodoDTO.java
@@ -0,0 +1,31 @@
+package com.leeseojune.todolist.dto;
+
+import com.leeseojune.todolist.entity.Todo;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TodoDTO {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Add{
+        private String content;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Todo{
+        private String content;
+        private boolean isCheck;
+
+        public Todo(com.leeseojune.todolist.entity.Todo todo){
+            this.content = todo.getContent();
+            this.isCheck = todo.isCheck();
+        }
+
+    }
+
+}
