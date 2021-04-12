791746fd3bafbd2eb414c5cf2894357d9a98144a
leeseojune <sung07288346@gmail.com>
2021-04-12 23:15:48 +0900

    UPDATE; TodoService

--- BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/service/TodoService.java
+++ BackEnd_Learn/Java/todolist/src/main/java/com/leeseojune/todolist/service/TodoService.java
@@ -3,8 +3,6 @@ package com.leeseojune.todolist.service;
 import com.leeseojune.todolist.dto.TodoDTO;
 import com.leeseojune.todolist.entity.Todo;
 
-import java.util.List;
-
 public interface TodoService {
     Iterable<Todo> getAll();
     void addTodo(TodoDTO.Add request);
