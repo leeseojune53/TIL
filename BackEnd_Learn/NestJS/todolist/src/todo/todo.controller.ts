34a6f9c39a439a1abbecd94c9c7b79683a06de77
leeseojune <sung07288346@gmail.com>
2021-04-15 23:05:13 +0900

    UPDATE; TodoController

--- BackEnd_Learn/NestJS/todolist/src/todo/todo.controller.ts
+++ BackEnd_Learn/NestJS/todolist/src/todo/todo.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { todoDTO } from './dto/todo.dto';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    constructor(private readonly todoService: TodoService){}
+
+    @Post()
+    async post(@Body() body: todoDTO){
+        this.todoService.add(body);
+    }
+
+    @Get()
+    async getAll() {
+        return this.todoService.getAll();
+    }
+
+    @Patch('check')
+    async check(@Query('id') id: number){
+        console.log(id);
+        this.todoService.check(id);
+    }
+
+}
