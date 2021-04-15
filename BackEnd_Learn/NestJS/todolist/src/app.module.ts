dc07943a808f1d14f470a2e4698654f8d874025f
leeseojune <sung07288346@gmail.com>
2021-04-15 23:03:46 +0900

    UPDATE; App module

--- BackEnd_Learn/NestJS/todolist/src/app.module.ts
+++ BackEnd_Learn/NestJS/todolist/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { connectionOptions } from './ormconfig';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    ConfigModule.forRoot({
+      load: [() => connectionOptions],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => config.get("development"),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
