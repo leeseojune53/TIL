50e37f8b3b2653788949a853567b3438472b789b
leeseojune <sung07288346@gmail.com>
2021-04-15 15:51:15 +0900

    CREATE; Todo Repository

---
+++ BackEnd_Learn/NestJS/todolist/src/todo/entity/todo.repository.ts
@@ -0,0 +1,11 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Todo } from "./todo.entity";
+
+@EntityRepository(Todo)
+export class TodoRepository extends Repository<Todo> {
+    public async findAll() {
+        return this.createQueryBuilder("todo")
+        .select("*")
+        .from("")
+    }
+}
