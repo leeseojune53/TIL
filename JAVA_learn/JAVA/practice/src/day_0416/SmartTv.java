65360c5e5e8100c98c4589a201213d556a164de4
leeseojune <sung07288346@gmail.com>
2021-04-16 20:25:49 +0900

    UPDATE; SmartTv

--- JAVA_learn/JAVA/practice/src/day_0416/SmartTv.java
+++ JAVA_learn/JAVA/practice/src/day_0416/SmartTv.java
@@ -1,5 +1,38 @@
 package day_0416;
 
-public class SmartTv {
+public class SmartTv implements RemoteControl, Searchable{
 
+	private int volume;
+
+	@Override
+	public void turnOn() {
+		System.out.println("Tv를 켭니다.");
+		
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("Tv를 끕니다.");
+		
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 TV 볼륨: " + this.volume);
+		
+	}
+
+	@Override
+	public void search(String url) {
+		System.out.println(url + "검색 완료");
+		
+	}
+	
 }
