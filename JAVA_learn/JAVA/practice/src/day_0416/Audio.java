cdd1e69584621314e7ee264b2c2e482f0eabb392
leeseojune <sung07288346@gmail.com>
2021-04-16 20:25:11 +0900

    UPDATE; Audio

--- JAVA_learn/JAVA/practice/src/day_0416/Audio.java
+++ JAVA_learn/JAVA/practice/src/day_0416/Audio.java
@@ -1,5 +1,43 @@
 package day_0416;
 
-public class Audio {
+public class Audio implements RemoteControl{
+	
+	private int volume;
+	private boolean mute;
+
+	@Override
+	public void turnOn() {
+		System.out.println("오디오 켜기");
+		
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("오디오 끄기");
+		
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		if(volume>RemoteControl.MAX_VOLUME) {
+			this.volume = RemoteControl.MAX_VOLUME;
+		} else if(volume<RemoteControl.MIN_VOLUME) {
+			this.volume = RemoteControl.MIN_VOLUME;
+		} else {
+			this.volume = volume;
+		}
+		System.out.println("현재 Audio 볼륨: " + this.volume);
+		
+	}
+	
+	@Override
+	public void setMute(boolean mute) {
+		this.mute = mute;
+		if (mute) {
+			System.out.println("Audio 무음 처리합니다.");
+		} else {
+			System.out.println("Audio 무음 해제합니다.");
+		}
+	}
 
 }
