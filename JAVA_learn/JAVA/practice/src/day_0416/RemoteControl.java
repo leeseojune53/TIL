94eec12150e9d36bdea16b0b1f393e285a98a0c1
leeseojune <sung07288346@gmail.com>
2021-04-16 20:25:21 +0900

    UPDATE; RemoteControl

--- JAVA_learn/JAVA/practice/src/day_0416/RemoteControl.java
+++ JAVA_learn/JAVA/practice/src/day_0416/RemoteControl.java
@@ -2,4 +2,25 @@ package day_0416;
 
 public interface RemoteControl {
 	
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+	
+	default void setMute(boolean mute) {
+		if(mute) {
+			System.out.println("무음 처리합니다.");
+		}else {
+			System.out.println("무음 해제합니다.");
+		}
+	}
+	
+	static void changeBattery() {
+		System.out.println("건전지를 교환합니다.");
+	}
+	
+	
+	
 }
