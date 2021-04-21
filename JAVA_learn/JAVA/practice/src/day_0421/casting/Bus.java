00e2b1d8c042931f240656904fdfda55e1dcff87
leeseojune <sung07288346@gmail.com>
2021-04-21 20:19:51 +0900

    CREATE; Bus

---
+++ JAVA_learn/JAVA/practice/src/day_0421/casting/Bus.java
@@ -0,0 +1,14 @@
+package day_0421.casting;
+
+public class Bus implements Vehicle {
+
+	@Override
+	public void run() {
+		System.out.println("버스가 달린다.");
+	}
+	
+	public void checkFare() {
+		System.out.println("승차요금을 체크한다.");
+	}
+
+}
