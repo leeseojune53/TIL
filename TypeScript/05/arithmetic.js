29081fad78ed104c7bec702dc8c545aecf6268b3
leeseojune <sung07288346@gmail.com>
2021-04-25 00:04:07 +0900

    CREATE; arithmetic

---
+++ TypeScript/05/arithmetic.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(10 + 2, 10 - 2, 10 * 2, 10 / 2, 10 % 2);
+console.log(Math.pow(10, 3));
+console.log(1 + "happy");
+console.log(false + "happy");
---
+++ TypeScript/05/arithmetic.ts
@@ -0,0 +1,5 @@
+console.log(10 + 2, 10 - 2, 10 * 2, 10 / 2, 10 % 2);
+console.log(10 ** 3);
+
+console.log(1 + "happy");
+console.log(false + "happy");
