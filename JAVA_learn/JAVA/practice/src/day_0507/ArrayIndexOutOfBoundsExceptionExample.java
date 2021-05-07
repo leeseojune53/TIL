81e17600698809433f43b51232660508829c58de
leeseojune <sung07288346@gmail.com>
2021-05-07 15:18:39 +0900

    CREATE; ArrayIndexOutOfBoundsExceptionExample

---
+++ JAVA_learn/JAVA/practice/src/day_0507/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,11 @@
+package day_0507;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+	public static void main(String[] args) {
+		String data1 = args[0];
+		String data2 = args[1];
+		
+		System.out.println("args[0]: " + data1);
+		System.out.println("args[1]: " + data2);
+	}
+}
