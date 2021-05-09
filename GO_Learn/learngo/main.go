4cd9d41821f77ddf5dd5b813d2ab7dd63a66312e
leeseojune <sung07288346@gmail.com>
2021-05-09 20:19:19 +0900

    CREATE; learngo

---
+++ GO_Learn/learngo/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return (name.)
+}
+
+func main() {
+	fmt.Println(multiply(2, 2))
+}
