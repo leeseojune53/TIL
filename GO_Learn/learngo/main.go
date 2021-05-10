601a37d16a7fb5d027287914b48fedfd4c4bc6c4
leeseojune <sung07288346@gmail.com>
2021-05-10 22:32:15 +0900

    UPDATE; main.go

--- GO_Learn/learngo/main.go
+++ GO_Learn/learngo/main.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func multiply(a int, b int) int {
 	return a * b
 }
 
-func lenAndUpper(name string) (int, string) {
-	return (name.)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	defer fmt.Println("I'm done?")
+	return
+}
+
+func superAdd(numbers ...int) int {
+	defer fmt.Println("I'm done!")
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	fmt.Println(multiply(2, 2))
+	fmt.Println(canIDrink(18))
 }
