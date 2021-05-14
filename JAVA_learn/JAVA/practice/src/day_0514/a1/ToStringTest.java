11eba4a082de5f4461717e336821d1bd0cfd8fde
leeseojune <sung07288346@gmail.com>
2021-05-14 15:05:02 +0900

    CREATE; ToStringTest

---
+++ JAVA_learn/JAVA/practice/src/day_0514/a1/ToStringTest.java
@@ -0,0 +1,13 @@
+package day_0514.a1;
+
+public class ToStringTest {
+
+	public static void main(String[] args) {
+		Object obj = new Object();
+		System.out.println(obj.toString());
+		
+		String name = new String("이름");
+		System.out.println(name);
+	}
+	
+}
