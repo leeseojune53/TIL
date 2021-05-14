40c775610698a2d2a40e53ea03e5d46a67af6210
leeseojune <sung07288346@gmail.com>
2021-05-14 15:20:35 +0900

    CREATE; Counter

---
+++ JAVA_learn/JAVA/practice/src/day_0514/a1/Counter.java
@@ -0,0 +1,16 @@
+package day_0514.a1;
+
+public class Counter {
+
+	private int no;
+	
+	public Counter(int no) {
+		this.no = no;
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println(no + "번 객체의 finalize()가 실행됨");
+	}
+	
+}
