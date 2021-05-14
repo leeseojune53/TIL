038ddb9784dedc3fd932e55d595e530e170e4494
leeseojune <sung07288346@gmail.com>
2021-05-14 14:33:30 +0900

    CREATE; ObjectTest

---
+++ JAVA_learn/JAVA/practice/src/day_0514/a1/ObjectTest.java
@@ -0,0 +1,15 @@
+package day_0514.a1;
+
+public class ObjectTest {
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		String
+		
+		boolean result1 = obj1.equals(obj2);
+		System.out.println(result1);
+		
+		boolean result2 = (obj1 == obj2);
+		System.out.println(result2);
+	}
+}
