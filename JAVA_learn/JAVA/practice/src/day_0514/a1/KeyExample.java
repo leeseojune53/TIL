2625565f0efba692d0c854a8f833365ba9a97f4d
leeseojune <sung07288346@gmail.com>
2021-05-14 14:33:25 +0900

    CREATE; java KeyExample

---
+++ JAVA_learn/JAVA/practice/src/day_0514/a1/KeyExample.java
@@ -0,0 +1,14 @@
+package day_0514.a1;
+
+import java.util.HashMap;
+
+public class KeyExample {
+	public static void main(String[] args) {
+		HashMap<Key, String> hashMap = new HashMap<Key, String>();
+		
+		hashMap.put(new Key(1), "홍길동");
+		
+		String value = hashMap.get(new Key(1));
+		System.out.println(value);
+	}
+}
