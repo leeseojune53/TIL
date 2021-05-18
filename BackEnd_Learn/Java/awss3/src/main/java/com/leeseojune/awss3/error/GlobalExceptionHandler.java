5dad590fa48798d9b8c36f4d32c0ddde96bae151
leeseojune <sung07288346@gmail.com>
2021-05-18 15:20:49 +0900

    CREATE; GlobalExceptionHandler

---
+++ BackEnd_Learn/Java/awss3/src/main/java/com/leeseojune/awss3/error/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.leeseojune.awss3.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MultipartException.class)
+    protected ResponseEntity<ErrorResponse> testHandler(MultipartException e) {
+        return new ResponseEntity<>(new ErrorResponse(200, "No File"), HttpStatus.OK);
+    }
+
+}
