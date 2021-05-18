2ce7d30bf8c6cc8ba75c4eb0a50d06e8b5c8d9a9
leeseojune <sung07288346@gmail.com>
2021-05-18 15:20:40 +0900

    CREATE; S3 ErrorResponse

---
+++ BackEnd_Learn/Java/awss3/src/main/java/com/leeseojune/awss3/error/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.leeseojune.awss3.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+}
