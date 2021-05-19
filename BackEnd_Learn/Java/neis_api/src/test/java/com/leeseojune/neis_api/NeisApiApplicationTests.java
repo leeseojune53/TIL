76cb3aa76ed80e3b6d86f4faf67e213117441ac8
leeseojune <sung07288346@gmail.com>
2021-05-19 23:31:17 +0900

    init NeisApiApplication

---
+++ BackEnd_Learn/Java/neis_api/src/test/java/com/leeseojune/neis_api/NeisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeseojune.neis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
