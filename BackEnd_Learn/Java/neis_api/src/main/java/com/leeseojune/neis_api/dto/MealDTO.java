57b625201f64a3a6a87d97865dcb7941fdd4b77e
leeseojune <sung07288346@gmail.com>
2021-05-19 23:31:50 +0900

    CREATE; MealDTO

---
+++ BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/dto/MealDTO.java
@@ -0,0 +1,12 @@
+package com.leeseojune.neis_api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MealDTO {
+    String schoolCode;
+    String ScCode;
+    String date;
+}
