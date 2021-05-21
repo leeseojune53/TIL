9d27515a4e9308ad3352e05a7bdcb61ec1f3e940
leeseojune <sung07288346@gmail.com>
2021-05-21 13:32:36 +0900

    UPDATE; SchoolInfoService

--- BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoService.java
+++ BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoService.java
@@ -1,12 +1,14 @@
 package com.leeseojune.neis_api.service;
 
+import com.leeseojune.neis_api.dto.MealDTO;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.List;
 
 public interface SchoolInfoService {
 
-    void getMeal(String schoolCode, String ScCode, String date) throws IOException, SAXException, ParserConfigurationException;
+    List<MealDTO.MealRes> getMeal(String schoolCode, String ScCode, String date) throws IOException, SAXException, ParserConfigurationException;
 
 }
