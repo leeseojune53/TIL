644a5aa9d69e97111a96fae4dd963698feb0c258
leeseojune <sung07288346@gmail.com>
2021-05-21 13:32:48 +0900

    UPDATE; SchoolInfoServiceImpl

--- BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoServiceImpl.java
+++ BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.leeseojune.neis_api.service;
 
+import com.leeseojune.neis_api.dto.MealDTO;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -11,6 +12,8 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SchoolInfoServiceImpl implements SchoolInfoService{
@@ -18,7 +21,7 @@ public class SchoolInfoServiceImpl implements SchoolInfoService{
     private final String baseUrl = "https://open.neis.go.kr/hub/mealServiceDietInfo";
 
     @Override
-    public void getMeal(String schoolCode, String scCode, String date) throws IOException, SAXException, ParserConfigurationException {
+    public List<MealDTO.MealRes> getMeal(String schoolCode, String scCode, String date) throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
         Document doc = dBuilder.parse(baseUrl + "?ATPT_OFCDC_SC_CODE=" + scCode + "&SD_SCHUL_CODE=" + schoolCode + "&MLSV_YMD=" + date);
@@ -27,19 +30,25 @@ public class SchoolInfoServiceImpl implements SchoolInfoService{
 
         NodeList nList = doc.getElementsByTagName("row");
 
+        List<MealDTO.MealRes> response = new ArrayList<>();
+
         for(int i=0, length = nList.getLength(); i < length; i++){
             Node node = nList.item(i);
             if(node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
-                System.out.println(getTagValue("MMEAL_SC_NM", element));
-                System.out.println(getTagValue("DDISH_NM", element));
+                List<String> menu = new ArrayList<>();
+                System.out.println();
+                String[] menus = getTagValue("DDISH_NM", element).split("[^\\uAC00-\\uD7AF\\u1100-\\u11FF\\u3130-\\u318F\n]");
+                for(String value : menus){
+                    if(value.length() != 0) {
+                        menu.add(value);
+                    }
+                }
+                response.add(new MealDTO.MealRes(getTagValue("MMEAL_SC_NM", element), menu));
             }
         }
+        return response;
 
-
-
-
-        System.out.println(nList.getLength());
     }
 
     private String getTagValue(String tag, Element element) {
@@ -47,7 +56,7 @@ public class SchoolInfoServiceImpl implements SchoolInfoService{
 
         Node nValue = (Node) nodeList.item(0);
         if(nValue == null)
-            return null;
+            return "";
         return nValue.getNodeValue();
     }
 
