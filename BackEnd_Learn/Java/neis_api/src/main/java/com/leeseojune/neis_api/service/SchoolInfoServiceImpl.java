f4cc6b5edecd33cc3509b7215c3d69efb2667ea3
leeseojune <sung07288346@gmail.com>
2021-05-21 14:36:49 +0900

    UPDATE; SchoolInfoServiceImpl
    code clean

--- BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoServiceImpl.java
+++ BackEnd_Learn/Java/neis_api/src/main/java/com/leeseojune/neis_api/service/SchoolInfoServiceImpl.java
@@ -18,43 +18,57 @@ import java.util.List;
 @Service
 public class SchoolInfoServiceImpl implements SchoolInfoService{
 
-    private final String baseUrl = "https://open.neis.go.kr/hub/mealServiceDietInfo";
+    private static final String BASEURL = "https://open.neis.go.kr/hub/mealServiceDietInfo";
 
     @Override
-    public List<MealDTO.MealRes> getMeal(String schoolCode, String scCode, String date) throws IOException, SAXException, ParserConfigurationException {
+    public MealDTO.MealRes getMeal(String schoolCode, String scCode, String date) throws IOException, SAXException, ParserConfigurationException {
         DocumentBuilderFactory dbFactoty = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactoty.newDocumentBuilder();
-        Document doc = dBuilder.parse(baseUrl + "?ATPT_OFCDC_SC_CODE=" + scCode + "&SD_SCHUL_CODE=" + schoolCode + "&MLSV_YMD=" + date);
+        Document doc = dBuilder.parse(BASEURL + "?ATPT_OFCDC_SC_CODE=" + scCode + "&SD_SCHUL_CODE=" + schoolCode + "&MLSV_YMD=" + date);
 
         doc.getDocumentElement().normalize();
 
         NodeList nList = doc.getElementsByTagName("row");
 
-        List<MealDTO.MealRes> response = new ArrayList<>();
+        MealDTO.MealRes response = new MealDTO.MealRes();
 
         for(int i=0, length = nList.getLength(); i < length; i++){
             Node node = nList.item(i);
             if(node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
+
                 List<String> menu = new ArrayList<>();
-                System.out.println();
                 String[] menus = getTagValue("DDISH_NM", element).split("[^\\uAC00-\\uD7AF\\u1100-\\u11FF\\u3130-\\u318F\n]");
+
                 for(String value : menus){
                     if(value.length() != 0) {
                         menu.add(value);
                     }
                 }
-                response.add(new MealDTO.MealRes(getTagValue("MMEAL_SC_NM", element), menu));
+
+                switch (getTagValue("MMEAL_SC_NM", element)){
+                    case "조식":
+                        response.setBreakfast(menu);
+                        break;
+                    case "중식":
+                        response.setLunch(menu);
+                        break;
+                    case "석식":
+                        response.setDinner(menu);
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        return response;
 
+        return response;
     }
 
     private String getTagValue(String tag, Element element) {
         NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
 
-        Node nValue = (Node) nodeList.item(0);
+        Node nValue = nodeList.item(0);
         if(nValue == null)
             return "";
         return nValue.getNodeValue();
