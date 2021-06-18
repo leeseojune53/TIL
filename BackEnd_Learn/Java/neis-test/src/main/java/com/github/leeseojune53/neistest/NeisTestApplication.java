415b7c0a24245152ee0e26be0b31c9ef71f3b59f
leeseojune <sung07288346@gmail.com>
2021-06-18 18:52:48 +0900

    init

---
+++ BackEnd_Learn/Java/neis-test/src/main/java/com/github/leeseojune53/neistest/NeisTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.leeseojune53.neistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeisTestApplication.class, args);
+    }
+
+}
---
+++ BackEnd_Learn/Java/neis-test/src/test/java/com/github/leeseojune53/neistest/NeisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leeseojune53.neistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
