99f6b2ef5609a2a5c0374a9b3f4d252b2b7b00b1
leeseojune <sung07288346@gmail.com>
2021-07-11 03:46:39 +0900

    Add 글로벌 필터

---
+++ BackEnd_Learn/Java/msa_learn/api-gateway/src/main/java/io/github/leeseojune53/apigateway/filter/GlobalFilter.java
@@ -0,0 +1,40 @@
+package io.github.leeseojune53.apigateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            log.info("GlobalFilter baseMessage>>>>>>" + config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("GlobalFilter Start>>>>>>" + exchange.getRequest());
+            }
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                if (config.isPostLogger()) {
+                    log.info("GlobalFilter End>>>>>>" + exchange.getResponse());
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+}
