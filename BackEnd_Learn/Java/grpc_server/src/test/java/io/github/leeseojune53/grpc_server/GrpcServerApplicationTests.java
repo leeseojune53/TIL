312f50f95f0d57d688bd21eba4d74c85d6b4e597
leeseojune <sung07288346@gmail.com>
2021-07-10 09:34:43 +0900

    init gRPC server

---
+++ BackEnd_Learn/Java/grpc_server/src/test/java/io/github/leeseojune53/grpc_server/GrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.grpc_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
