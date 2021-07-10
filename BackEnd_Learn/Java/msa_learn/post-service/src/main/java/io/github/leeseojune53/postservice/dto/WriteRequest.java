a3dc86314e1f5b3e834a96f118e75a4688bc7fb5
leeseojune <sung07288346@gmail.com>
2021-07-11 03:48:03 +0900

    Add 글쓰기 DTO

---
+++ BackEnd_Learn/Java/msa_learn/post-service/src/main/java/io/github/leeseojune53/postservice/dto/WriteRequest.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.postservice.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class WriteRequest {
+
+    private Integer id;
+    private String value;
+
+}
