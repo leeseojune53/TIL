88b472948660ac456058512f0aa59d07486ecb30
leeseojune <sung07288346@gmail.com>
2021-07-11 03:26:16 +0900

    init projects

---
+++ BackEnd_Learn/Java/msa_learn/post-service/src/test/java/io/github/leeseojune53/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
