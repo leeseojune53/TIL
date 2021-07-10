1065f08c92cb404a62f8903f7b555092904dc51d
leeseojune <sung07288346@gmail.com>
2021-07-10 09:33:26 +0900

    init gRPC client

---
+++ BackEnd_Learn/Java/grpc/src/main/java/io/github/leeseojune53/grpc/GRpcApplication.java
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcApplication.class, args);
+    }
+
+}
