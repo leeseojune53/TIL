bddc25636ac27ac309f3107db1f86dcfeabad6fc
leeseojune <sung07288346@gmail.com>
2021-07-12 23:40:33 +0900

    Add AuthDetails Service

---
+++ BackEnd_Learn/Java/auth/src/main/java/io/github/leeseojune53/auth/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package io.github.leeseojune53.auth.security.auth;
+
+import io.github.leeseojune53.auth.entity.user.UserRepository;
+import io.github.leeseojune53.auth.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+        return userRepository.findById(id)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+}
