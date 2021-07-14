4b76213447c85ef291fe7fba2e635e49de97db76
leeseojune <sung07288346@gmail.com>
2021-07-14 22:51:37 +0900

    Add User DAO

---
+++ BackEnd_Learn/kotlin/auth/src/main/kotlin/io/github/leeseojune53/auth/entity/UserRepository.kt
@@ -0,0 +1,8 @@
+package io.github.leeseojune53.auth.entity
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository: CrudRepository<User, Long> {
+}
