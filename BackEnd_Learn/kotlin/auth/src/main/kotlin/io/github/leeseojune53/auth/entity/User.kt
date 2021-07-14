94c7140b5193c87be37599b63fef98c8218b4e91
leeseojune <sung07288346@gmail.com>
2021-07-14 22:50:28 +0900

    Add User Model

---
+++ BackEnd_Learn/kotlin/auth/src/main/kotlin/io/github/leeseojune53/auth/entity/User.kt
@@ -0,0 +1,51 @@
+package io.github.leeseojune53.auth.entity
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import java.util.stream.Collectors
+import javax.persistence.*
+
+
+@Entity
+class User(
+    @Id
+    private var name: String,
+    private var password: String,
+    @Enumerated(EnumType.STRING)
+    @Column
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "role", joinColumns = arrayOf(JoinColumn(name = "name")))
+    private var roles : MutableList<Role>
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val rolesToString = roles.stream().map { obj: Role -> obj.name }.collect(Collectors.toList())
+        return rolesToString.stream().map { role: String -> SimpleGrantedAuthority(role) }.collect(Collectors.toList())
+    }
+
+    override fun getPassword(): String {
+        return this.password
+    }
+
+    override fun getUsername(): String {
+        return this.name
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+    constructor(name: String, password: String) : this(name, password, null!!)
+}
