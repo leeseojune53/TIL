dd2f547ec8224d54f109a5af4fb861bef65f1d07
leeseojune <sung07288346@gmail.com>
2021-07-16 00:04:15 +0900

    Update Id Type

--- BackEnd_Learn/kotlin/auth/src/main/kotlin/io/github/leeseojune53/auth/entity/UserRepository.kt
+++ BackEnd_Learn/kotlin/auth/src/main/kotlin/io/github/leeseojune53/auth/entity/UserRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository: CrudRepository<User, Long> {
+interface UserRepository: CrudRepository<User, String> {
 }
