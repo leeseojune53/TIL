b7290c1ecdc7ca7849f87058ee5e54fbd471d2aa
leeseojune <sung07288346@gmail.com>
2021-12-06 22:39:13 +0900

    📑 :: init

---
+++ BackEnd_Learn/kotlin/kopring/src/test/kotlin/io/github/leeseojune53/kopring/KopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
