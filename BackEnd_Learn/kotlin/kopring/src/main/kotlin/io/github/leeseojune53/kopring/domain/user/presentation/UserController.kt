e7eafe7b3dcef504e2408cbac293931f2e092b62
leeseojune <sung07288346@gmail.com>
2021-12-07 15:19:33 +0900

    📑 :: UserController

---
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/domain/user/presentation/UserController.kt
@@ -0,0 +1,22 @@
+package io.github.leeseojune53.kopring.domain.user.presentation
+
+import io.github.leeseojune53.kopring.domain.user.presentation.dto.request.UserRequest
+import io.github.leeseojune53.kopring.domain.user.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserController(
+		private val userService: UserService,
+) {
+
+	@PostMapping("/register")
+	@ResponseStatus(HttpStatus.CREATED)
+	fun signUp(@RequestBody request: UserRequest) {
+		userService.saveUser(request)
+	}
+
+}
