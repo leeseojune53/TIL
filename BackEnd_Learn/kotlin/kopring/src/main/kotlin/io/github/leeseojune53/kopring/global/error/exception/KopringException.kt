aaa9b1f1b34a865c66d383890e197f8af322c736
leeseojune <sung07288346@gmail.com>
2021-12-07 15:11:57 +0900

    📑 :: KopringException

---
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/global/error/exception/KopringException.kt
@@ -0,0 +1,11 @@
+package io.github.leeseojune53.kopring.global.error.exception
+
+open class KopringException(
+		private val errorCode: ErrorCode
+) : RuntimeException() {
+	val status: Int
+	get() = errorCode.status
+
+	val errorMessage: String
+	get() = errorCode.message
+}
