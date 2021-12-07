3ace990a30b694e01f94c028437731ba8c47d1d0
leeseojune <sung07288346@gmail.com>
2021-12-07 15:04:08 +0900

    📑 :: ErrorCode

---
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/global/error/exception/ErrorCode.kt
@@ -0,0 +1,13 @@
+package io.github.leeseojune53.kopring.global.error.exception
+
+import com.fasterxml.jackson.annotation.JsonFormat
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+enum class ErrorCode(
+		val status: Int,
+		val message: String
+) {
+
+	USER_NOT_FOUND(400, "User not found.")
+
+}
