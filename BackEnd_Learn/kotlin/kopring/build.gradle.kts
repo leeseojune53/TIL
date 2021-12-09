6d201009fde901e0403d5e76c9f1479badf73683
leeseojune <sung07288346@gmail.com>
2021-12-09 20:43:55 +0900

    ⚡ :: redis dependency

--- BackEnd_Learn/kotlin/kopring/build.gradle.kts
+++ BackEnd_Learn/kotlin/kopring/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	runtimeOnly("mysql:mysql-connector-java")
 
 	implementation("org.springframework.boot:spring-boot-starter-security")
