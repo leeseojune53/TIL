f8ee7fe7cc1be44e55db7fcab920f1b16d987683
leeseojune <sung07288346@gmail.com>
2021-12-09 19:28:18 +0900

    📑 :: SecurityConfig

---
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/global/config/SecurityConfig.kt
@@ -0,0 +1,38 @@
+package io.github.leeseojune53.kopring.global.config
+
+import io.github.leeseojune53.kopring.global.security.jwt.JwtTokenFilter
+import io.github.leeseojune53.kopring.global.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsUtils
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+) : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http
+            .formLogin().disable()
+            .cors().and()
+            .csrf().disable()
+        http
+            .authorizeRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+            .antMatchers(HttpMethod.POST, "/register").permitAll()
+            .antMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated()
+            .apply { FilterConfig(JwtTokenFilter(jwtTokenProvider)) }
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+}
