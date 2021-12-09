507b1ac3de2395d9fbc676dd28a34a13c6249a1d
leeseojune <sung07288346@gmail.com>
2021-12-09 19:24:45 +0900

    ⚡ :: JwtTokenProvider

--- BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/global/security/jwt/JwtTokenProvider.kt
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/global/security/jwt/JwtTokenProvider.kt
@@ -1,13 +1,19 @@
 package io.github.leeseojune53.kopring.global.security.jwt
 
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.github.leeseojune53.kopring.global.security.auth.AuthDetailsService
+import io.github.leeseojune53.kopring.global.security.exception.ExpiredTokenException
+import io.github.leeseojune53.kopring.global.security.exception.InvalidTokenException
+import io.jsonwebtoken.*
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 import java.util.*
+import javax.servlet.http.HttpServletRequest
 
 @Component
 class JwtTokenProvider(
     private val jwtProperties: JwtProperties,
+    private val authDetailsService: AuthDetailsService,
 ) {
 
     fun generateAccessToken(name: String): String =
@@ -15,6 +21,35 @@ class JwtTokenProvider(
 
     fun generateRefreshToken(name: String): String =
         generateToken(name, jwtProperties.getAccessExp(), JwtProperties.REFRESH_TYPE)
+
+    fun resolveToken(request: HttpServletRequest): String? {
+        val bearer: String? = request.getHeader(jwtProperties.header)
+        if (bearer != null && bearer.startsWith(jwtProperties.prefix) && bearer.length >= jwtProperties.prefix.length + 1) {
+            return bearer.substring(jwtProperties.prefix.length + 1)
+        }
+        return null
+    }
+
+    fun getAuthentication(token: String?): Authentication? {
+        return token?.let { it ->
+            val claims = getClaims(it)
+            val userDetails = authDetailsService.loadUserByUsername(claims.subject)
+
+            return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+        }
+    }
+
+    private fun getClaims(token: String): Claims {
+        return try {
+            Jwts.parser().setSigningKey(jwtProperties.secret)
+                .parseClaimsJws(token).body
+        } catch (e: Exception) {
+            when(e) {
+                is ExpiredJwtException -> throw ExpiredTokenException.EXCEPTION
+                else -> throw InvalidTokenException.EXCEPTION
+            }
+        }
+    }
 
     private fun generateToken(name: String, exp: Long, typ: String): String {
         return Jwts.builder()
