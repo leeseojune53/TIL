8c068a02c227172b87ab0bbe0a5b9c0a84a6480d
leeseojune <sung07288346@gmail.com>
2021-12-10 14:12:20 +0900

    ⚡ :: findByToken

--- BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/domain/refresh_token/domain/repositories/RefreshTokenRepository.kt
+++ BackEnd_Learn/kotlin/kopring/src/main/kotlin/io/github/leeseojune53/kopring/domain/refresh_token/domain/repositories/RefreshTokenRepository.kt
@@ -4,4 +4,5 @@ import io.github.leeseojune53.kopring.domain.refresh_token.domain.RefreshToken
 import org.springframework.data.repository.CrudRepository
 
 interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
+    fun findByToken(token: String): RefreshToken?
 }
