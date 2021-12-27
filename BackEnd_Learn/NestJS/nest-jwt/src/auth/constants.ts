fe485a5e4c62117826e9b581af258215facb837f
leeseojune <sung07288346@gmail.com>
2021-12-27 20:28:36 +0900

    📑 :: Jwt constants

--- BackEnd_Learn/NestJS/nest-jwt/src/auth/constants.ts
+++ BackEnd_Learn/NestJS/nest-jwt/src/auth/constants.ts
@@ -1,3 +1,9 @@
-export const jwtConstants = {
-    secret: 'secretKey',
-};
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export const JWT_SECRET = process.env.PRODUCTION_JWT_SECRET;
+
+export const JWT_ACCESS_EXP = process.env.PRODUCTION_ACCESS_EXP;
+
+export const JWT_REFRESH_EXP = process.env.PRODUCTION_REFRESH_EXP;
