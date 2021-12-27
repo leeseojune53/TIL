47133e2a28389c0ccc327b66174e29061d43f941
leeseojune <sung07288346@gmail.com>
2021-12-27 20:31:42 +0900

    ♻ :: 모듈 위치 변경

--- BackEnd_Learn/NestJS/nest-jwt/src/auth/auth.module.ts
+++ BackEnd_Learn/NestJS/nest-jwt/src/auth/auth.module.ts
@@ -1,18 +1,20 @@
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { UsersModule } from 'src/users/users.module';
-import { AuthService } from './auth.service';
-import { jwtConstants } from './constants';
+import {Module} from '@nestjs/common';
+import {JwtModule} from '@nestjs/jwt';
+import {UsersModule} from 'src/users/users.module';
+import {AuthService} from './auth.service';
+import {JwtStrategy} from "../shared/auth/jwt.strategy";
+import {PassportModule} from "@nestjs/passport";
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: {expiresIn: '60s'},
-    }),
-  ],
-  providers: [AuthService],
-  exports: [AuthService, JwtModule]
+    imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({}),
+    ],
+    providers: [AuthService, JwtStrategy],
+    exports: [AuthService],
+    controllers: [AuthController]
 })
-export class AuthModule {}
+export class AuthModule {
+}
