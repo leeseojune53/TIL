fbc93b08aa321e8a87afd297387e4838e9b60ab4
leeseojune <sung07288346@gmail.com>
2021-12-27 20:28:58 +0900

    📑 :: orm config

---
+++ BackEnd_Learn/NestJS/nest-jwt/src/typeorm/ormconfig.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import {ConnectionOptions} from "typeorm";
+
+
+interface DBConnectionOptions {
+    [env: string]: ConnectionOptions;
+}
+
+const connectionOptions: DBConnectionOptions = {
+    development: {
+        type: "mysql",
+        host: process.env.DEVELOPMENT_DATABASE_HOST,
+        port: +process.env.DEVELOPMENT_DATABASE_PORT,
+        username: "asdf",
+        password: process.env.DEVELOPMENT_DATABASE_PASSWORD,
+        database: process.env.DEVELOPMENT_DATABASE_NAME,
+        synchronize: true,
+        logging: true,
+        entities: ["./dist/**/*.entity.js"],
+    },
+    production: {
+        type: "mysql",
+        host: process.env.PRODUCTION_DATABASE_HOST,
+        port: +process.env.PRODUCTION_DATABASE_PORT,
+        username: process.env.PRODUCTION_DATABASE_USER,
+        password: process.env.PRODUCTION_DATABASE_PASSWORD,
+        database: process.env.PRODUCTION_DATABASE_NAME,
+        synchronize: true,
+        logging: true,
+        entities: ["./dist/**/*.entity.js"],
+    },
+};
+
+export {connectionOptions}
