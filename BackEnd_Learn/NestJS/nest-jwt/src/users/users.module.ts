8853d7f769f7f5b1a51da9e4873266fe26d41d1e
leeseojune <sung07288346@gmail.com>
2021-12-27 20:32:35 +0900

    📑 :: User module

--- BackEnd_Learn/NestJS/nest-jwt/src/users/users.module.ts
+++ BackEnd_Learn/NestJS/nest-jwt/src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user.entity';
 import { UsersService } from './users.service';
+import {UserRepository} from "../shared/user/entity/user.repository";
+import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   exports: [TypeOrmModule, UsersService],
-  providers: [UsersService]
+  providers: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
