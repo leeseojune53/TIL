293b8ad174cdad438540f017e7829aed66216d1e
Lee SeoJune <sung07288346@gmail.com>
2022-01-30 19:51:05 +0900

    📑 :: Todolist

---
+++ iOS_Learn/TodoList/TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by 이서준 on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
