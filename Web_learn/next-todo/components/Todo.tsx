1a175fe9dd27cc84d681460de9ca9f453570f2ff
Lee SeoJune <sung07288346@gmail.com>
2022-02-23 22:38:05 +0900

    📑 :: TodoComponent

---
+++ Web_learn/next-todo/components/Todo.tsx
@@ -0,0 +1,37 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const FlexBox = styled.div`
+  display: flex;
+`;
+
+const Box = styled(FlexBox)`
+  align-items: center;
+  border: 1px solid #000000;
+  border-radius: 10px;
+  width: 300px;
+`;
+
+const Textbox = styled.div`
+  font-size: 24px;
+  padding: 15px;
+  text-align: center;
+`;
+
+interface ViewProps {
+  todo: string;
+}
+export default function View({ todo }: ViewProps) {
+  return (
+    <Container>
+      <Box>
+        <input type="checkbox" />
+        <Textbox>{todo}</Textbox>
+      </Box>
+    </Container>
+  );
+}
