48d3652b6c0af428b8ba45c2ef378248a1a48552
Lee SeoJune <sung07288346@gmail.com>
2022-02-25 17:30:33 +0900

    ⚡️ :: 삭제, 체크 추가

--- Web_learn/next-todo/pages/index.tsx
+++ Web_learn/next-todo/pages/index.tsx
@@ -6,17 +6,42 @@ import InputBox from "../components/Inputbox";
 import Todo from "../components/Todo";
 import styles from "../styles/Home.module.css";
 
+export type TodoType = {
+  id: number;
+  value: string;
+  checked: boolean;
+};
+
 const Home: NextPage = () => {
-  const [todoList, setTodoList] = useState<string[]>([]);
+  let id = 1;
+  const [todoList, setTodoList] = useState<TodoType[]>([]);
+
   const addValue = (value: string) => {
-    setTodoList([...todoList, value]);
+    setTodoList([...todoList, { id: id++, value: value, checked: false }]);
+  };
+
+  const onCheck = (id: number) => {
+    let todo = todoList.find((element) => element.id === id);
+    if (todo) {
+      todo.checked = !todo.checked;
+    } else {
+      alert("존재하지 않는 Todo입니다.");
+    }
+  };
+
+  const onDelete = (id: number) => {
+    setTodoList(
+      todoList.filter((element) => {
+        element.id === id;
+      })
+    );
   };
 
   return (
     <div>
       <InputBox addValue={addValue}></InputBox>
       {todoList.map((value) => (
-        <Todo todo={value} />
+        <Todo todo={value} onCheck={onCheck} onDelete={onDelete} />
       ))}
     </div>
   );
